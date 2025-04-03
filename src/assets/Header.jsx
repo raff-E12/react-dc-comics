a204e3e79109afe66b9fd03f43c7a1c5c2dbeb4c
Raff_12 <raffelecoppola381@gmail.com>
2025-04-03 18:38:33 +0200

    feat: Completamento della Navbar

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -7,19 +7,22 @@ export default function Header() {
      <div className='icon-logo'>
       <img src="public/img/dc-logo.png" alt="" title='Benvenuto' aria-label='logo'/>
      </div>
-     <div className='nav-links-hd container-flex-fluid col-xxl-7 col-2xl-5' aria-label='Header-Links-Nav'>
+     <div className='nav-links-hd container-flex-fluid col-xxl-7 col-2xl-5 col-xl-3' aria-label='Header-Links-Nav'>
       <ul className='container-flex col-sm-12 list-nav'>
-        <li><a href="#">Charcters</a></li>
-        <li><a href="#">Comics</a></li>
-        <li><a href="#">Movies</a></li>
-        <li><a href="#">TV</a></li>
-        <li><a href="#">Games</a></li>
-        <li><a href="#">Collectibles</a></li>
-        <li><a href="#">Videos</a></li>
-        <li><a href="#">Fans</a></li>
-        <li><a href="#">News</a></li>
-        <li><a href="#">Shop</a></li>
+        <li><a href="#" className='links'>Charcters</a></li>
+        <li><a href="#" className='links'>Comics</a></li>
+        <li><a href="#" className='links'>Movies</a></li>
+        <li><a href="#" className='links'>TV</a></li>
+        <li><a href="#" className='links'>Games</a></li>
+        <li><a href="#" className='links'>Collectibles</a></li>
+        <li><a href="#" className='links'>Videos</a></li>
+        <li><a href="#" className='links'>Fans</a></li>
+        <li><a href="#" className='links'>News</a></li>
+        <li><a href="#" className='links'>Shop</a></li>
       </ul>
+     </div>
+     <div className='ham-icon'>
+        <i class="fa-solid fa-bars"></i>
      </div>
    </header>
    </>
