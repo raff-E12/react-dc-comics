9219ce24ef1a50ec26358f8ecd9f03c2dece108d
Raff_12 <raffelecoppola381@gmail.com>
2025-04-08 11:14:43 +0200

    feat: Modifica preliminare del layout

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -3,9 +3,7 @@ import Navlinks from '../components/Navlinks'
 
 export default function Header(props) {
   const list_links = props.links;
-  let set_click_indicator = true;
-  // console.log(set_click_indicator)
-  const IsopenHandleClick = (value) => { return value = false};
+  const [isOpen, SetOpen] = useState(false);
   return (
    <>
    <header className='header-sc container-flex-fluid col-sm-12 col-xxl-12' aria-label='Header-Section'>
@@ -17,12 +15,12 @@ export default function Header(props) {
         <Navlinks list={list_links}/>
       </div>
       <div className='box-icon ham'>
-          <i class="fa-solid fa-bars" onClick={()=>{IsopenHandleClick(set_click_indicator)}}></i>
+          <i class="fa-solid fa-bars" onClick={()=>{SetOpen(value => !value)}}></i>
       </div>
       </div>
-      <div className={`ham-box-left ${set_click_indicator ? "show" : "aside"}`}>
+      <div className={`ham-box-left ${isOpen ? "show" : "aside"}`}>
          <div className='col-ham'>
-            <i class="fa-solid fa-xmark" onClick={() => {IsopenHandleClick(set_click_indicator)}}></i>
+            <i class="fa-solid fa-xmark" onClick={() => {SetOpen(value => !value)}}></i>
          </div>
          <Navlinks list={list_links}/>
       </div>
